ff3020147b1bcc8c9c9f91505940d5c7472a7063
kimsu10 <betnwjd25@gmail.com>
2025-12-09 20:42:01 +0900

    feat: add rehype-raw * rehype-highlight * make custom style * set local test

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import rehypeHighlight from "rehype-highlight";
+import "../style/custom.css";
 
 /**
  * Markdown Rendering
@@ -9,8 +12,20 @@ import ReactMarkdown from "react-markdown";
 const Post = ({ category, fileName }) => {
   const [content, setContent] = useState("");
 
+  // 레포지토리 public 후 사용가능 => 작동 ✅
+  // useEffect(() => {
+  //   const url = `https://raw.githubusercontent.com/Kimsu10/my-dev-life/main/src/posts/${category}/${fileName}.md`;
+
+  //   fetch(url)
+  //     .then((res) => res.text())
+  //     .then((text) => {
+  //       setContent(text);
+  //     });
+  // }, [fileName]);
+
+  // 로컬 테스트용
   useEffect(() => {
-    const url = `https://raw.githubusercontent.com/Kimsu10/my-dev-life/main/src/posts/${category}/${fileName}.md`;
+    const url = `/posts/${category}/${fileName}.md`;
 
     fetch(url)
       .then((res) => res.text())
@@ -20,8 +35,10 @@ const Post = ({ category, fileName }) => {
   }, [fileName]);
 
   return (
-    <div style={{ maxWidth: 800, margin: "auto", padding: 20 }}>
-      <ReactMarkdown>{content}</ReactMarkdown>
+    <div className="markdown-body">
+      <ReactMarkdown rehypePlugins={[rehypeRaw, rehypeHighlight]}>
+        {content}
+      </ReactMarkdown>
     </div>
   );
 };
